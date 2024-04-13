aaa306e1df2626ce4de270bcf19a535ee0006b2f
kkkkiiiiii <98201682+kkkkiiiiii@users.noreply.github.com>
2024-04-13 17:31:48 +0900

    Done : Guessing Number Game

--- RiderProjects/cpp_example/GuessingNumberGame/GuessingNumberGame.cpp
+++ RiderProjects/cpp_example/GuessingNumberGame/GuessingNumberGame.cpp
@@ -1,6 +1,8 @@
+#include "NumberGame.h"
 
 int main(int argc, char* argv[])
 {
-    
+    NumberGame number_game;
+    number_game.playGame();
     return 0;
 }
--- RiderProjects/cpp_example/GuessingNumberGame/NumberGame.h
+++ RiderProjects/cpp_example/GuessingNumberGame/NumberGame.h
@@ -1,7 +1,21 @@
-﻿#pragma once
+﻿#ifndef GUESSINGGAME
+#define GUESSINGGAME
 
+#include <iostream>
+using namespace std;
+// Guessing game class definition
 class NumberGame
 {
+private:
+    int numberToGuess;
+    int numberOfAttempts;
 public:
-    
+    NumberGame();
+    ~NumberGame();
+    void playGame();
+    bool isCorrectGuessing(int guessNum);
 };
+
+#endif
+
+
