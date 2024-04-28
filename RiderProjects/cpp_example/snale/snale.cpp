6a89d3fed3cb746dd086341bdd192a857805ca59
kkkkiiiiii <alohomoratolit@naver.com>
2024-04-28 23:27:37 +0900

    feat : PrintBoard
    iomanip

--- RiderProjects/cpp_example/snale/snale.cpp
+++ RiderProjects/cpp_example/snale/snale.cpp
@@ -1,6 +1,29 @@
+#include <iomanip>
+#include <iostream>
+
+using namespace std;
+
+const int MAX = 100;
+int board[MAX][MAX];
+int N;
+
+void PrintBoard()
+{
+    for (int y = 0; y < N; y++)
+    {
+        for(int x = 0; x < N; x++)
+        {
+            cout << setfill('0') << setw(2) << board[y][x] << " ";
+        }
+        cout << endl;
+    }
+}
 
 int main(int argc, char* argv[])
 {
+    cin >> N;
+
+    PrintBoard();
     
     return 0;
 }
