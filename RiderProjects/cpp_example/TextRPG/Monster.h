699705fe10d92383834199735abf549637036b77
kkkkiiiiii <alohomoratolit@naver.com>
2024-06-23 00:28:20 +0900

    Create Monster.h

---
+++ RiderProjects/cpp_example/TextRPG/Monster.h
@@ -0,0 +1,28 @@
+﻿#ifndef MONSTER_H
+#define MONSTER_H
+
+#include <iostream>
+#include <string>
+using namespace std;
+
+class Monster
+{
+private:
+    string name;
+    int health;
+    int attackPower;
+public:
+    // 생성자
+    Monster(string monsterName, int hp, int attack);
+
+    // 공격 메서드
+    void attack();
+
+    // 데미지를 받는 메서드
+    void takeDamage(int damage);
+
+    // 현재 상태 출력 메서드
+    void displayStatus();
+};
+
+#endif
