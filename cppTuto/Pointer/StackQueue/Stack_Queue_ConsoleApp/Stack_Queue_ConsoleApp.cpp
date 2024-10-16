86749fc4c33cca2ad6405ba0b1053327fe091bf7
aloHyomora <alohomoratolit@naver.com>
2024-10-16 23:03:08 +0900

    [Add] : Create Stack_Queue example

---
+++ cppTuto/Pointer/StackQueue/Stack_Queue_ConsoleApp/Stack_Queue_ConsoleApp.cpp
@@ -0,0 +1,18 @@
+﻿#include <iostream>
+
+using namespace std;
+
+
+int main()
+{
+    // Vector
+    // [ ][ ][ ][ ]
+
+    // List
+    // [ ] <-> [ ] <-> [ ] <-> [ ]
+
+    // Stack -> 선입선출 FIFO -> First In First Out
+    // 벡터를 이용해서 Stack을 구현한다? -> [ ][ ][ ][ ]
+
+    // Queue -> 후입선출 LIFO -> Last In First Out
+}
