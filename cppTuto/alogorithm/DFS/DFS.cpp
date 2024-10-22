d75ef60dc5f9b1f718a1b99b4251f0b9fbab59d7
aloHyomora <alohomoratolit@naver.com>
2024-10-22 18:28:04 +0900

    [Add] : Dijikstra 다익스트라
    - BFS + 양념(cost)

--- cppTuto/alogorithm/DFS/DFS.cpp
+++ cppTuto/alogorithm/DFS/DFS.cpp
@@ -19,7 +19,7 @@ struct Vertex {
 };
 
 vector<Vertex> vertices;
-vector<vector<int >> adjacent;
+vector<vector<int>> adjacent;
 vector<bool> visited;
 
 // 발견한
@@ -146,6 +146,83 @@ void CreateGraph1() {
     };
 }
 
+// 다 익스트라: 발견해놓은 걸 기록해두고 더 좋은 경로를 선택한다.
+// 예약 시스템
+void CreateGraph() {
+    vertices.resize(6);
+
+    // 가중치
+    adjacent = vector<vector<int>>(6, vector<int>(6, -1));
+    adjacent[0][1] = adjacent[1][0] = 15;
+    adjacent[0][3] = adjacent[3][0] = 35;
+    adjacent[1][2] = adjacent[2][1] = 5;
+    adjacent[1][3] = adjacent[1][3] = 10;
+    adjacent[3][4] = adjacent[4][3] = 5;
+    adjacent[4][5] = adjacent[5][4] = 5;
+}
+
+struct VertexCost {
+    VertexCost(int cost, int vertex) : cost(cost), vertex(vertex) {}
+
+    bool operator<(const VertexCost& other) const {
+        return cost < other.cost;
+     }
+    bool operator>(const VertexCost& other) const {
+        return cost > other.cost;
+    }
+
+    int cost;
+    int vertex;
+};
+
+// BFS + 양념(cost)
+void Dijikstra(int here) {
+    priority_queue<VertexCost, vector<VertexCost>, greater<VertexCost>> pq;
+
+    // 각 정점 별로 최소 거리
+
+    // best case 관리
+    vector<int> best(6, INT32_MAX);
+    vector<int> parent(6, -1);
+
+    pq.push(VertexCost(0, here));
+    best[here] = 0;
+    parent[here] = here;
+
+    while (pq.empty() == false)
+    {
+        // 제일 좋은 후보를 찾는다.
+        VertexCost v = pq.top();
+        pq.pop();
+
+        int cost = v.cost;
+        here = v.vertex;
+
+        // 더 짧은 경로를 뒤늦게 찾았다면 스킵
+        if (best[here] < cost)
+            continue;
+
+        // 방문
+        cout << "방문! " << here << endl;
+
+        for (int there = 0; there < 6; there++)
+        {
+            // 연결되지 않았으면 스킵
+            if (adjacent[here][there] == -1)
+                continue;
+
+            // 더 좋은 경로를 과거에 찾았으면 스킵
+            int nextCost = best[here] + adjacent[here][there];
+            if (nextCost >= best[there])
+                continue;
+
+            // 지금까지 찾은 경로 중에서는 최선의 수치 = 갱신
+            best[there] = nextCost;
+            parent[there] = here;   // 나중에 갱신될 수 있음
+            pq.push(VertexCost(nextCost, there));
+        }
+    }
+}
 
 int main()
 {
@@ -155,6 +232,8 @@ int main()
 
     discorvered = vector<bool>(6, false);
     //Dfs(0);
-    Bfs(0);
+    // Bfs(0);
+
+    Dijikstra(0);
 
 }
