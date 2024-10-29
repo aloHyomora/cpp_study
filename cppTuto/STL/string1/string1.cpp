6bd2fd39007a637e10ae35a150015db1fe6ba528
aloHyomora <alohomoratolit@naver.com>
2024-10-29 23:58:06 +0900

    [Add] : 유니코드

--- cppTuto/STL/string1/string1.cpp
+++ cppTuto/STL/string1/string1.cpp
@@ -60,8 +60,35 @@ int main()
 
 	string str4 = str.substr(0, 3);
 
-	const char* name = str.c_str();
+	const char* name1 = str.c_str();
 #pragma endregion
 
+	// 1) ASCII 코드 -> 영어만 고려
+	// char(1) 0~255
+	char a1 = 'A';
+	// 7bit (0~127)
 
+	// 2) ANSI << 동일한 키코드 = 동일한 문자
+	// ASCII + 각국 언어별로 128
+	// CP949(한국어 포맷)
+
+	// 3) 유니코드 (동일 번호 = 동일한 문자 = 동일한 유니코드)
+	// - 인코딩 (utf8, utf16)
+	// - utf8 : 영어(1바이트), 한국어/중국어(3바이트)
+	// - utf16 : 영어(2바이트), 한국(2),중국(2)	-> 동시 서비스라면 utf16으로!
+
+	// 문자는 무조건 1바이트다. 는 틀렸다.
+
+	setlocale(LC_ALL, "");
+	cout << "LC_ALL: " << setlocale(LC_ALL, NULL) << endl;	// 윈도우 언어가 한글
+
+	// ? 이걸 중국에서 보면 깨질 수 있다. 왜냐하면 중국 쪽 포맷에는 대응되지 않는 키코드가 있기 때문.
+	const char* test = "aaa한국어";
+	cout << test << endl;
+
+	auto test3 = u8"aaa김효입니다.";	// utf8로 인코딩
+	setlocale(LC_ALL, "en_US.UTF-8"); // utf-8로 맞춰주기
+	cout << test3 << endl;
+
+	auto test4 = u"aaa김효입니다";
 }
